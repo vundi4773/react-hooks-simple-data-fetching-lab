59f328ad9ac8c079429f255ba875b49ddbf3bb33
vundi <vundi4773@gmail.com>
2024-06-15 00:46:27 +0300

    data fetched

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [image, setImage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data.message);
+        setIsLoading(true);
+      });
+  }, []);
+  if (!isLoading) return <p>Loading...</p>;
+  return (
+    <div>
+      <img src={image} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
